7cb152e9dcfc8cbe3b2d047c4878275682068706
Amiel-Saballo <amiel.saballo@awsys-i.com>
2026-06-08 15:23:43 +0800

    Added Trainee B hobby

--- Hobby.java
+++ Hobby.java
@@ -5,8 +5,8 @@ public class Hobby {
 		displayTraineeHobbyC();
 	}
 
-	public static void displayTraineeHobbyX() {
-		System.out.println("Trainee X’s Hobbies:");
-		// Hobbies of Trainee X will be added here
+	public static void displayTraineeHobbyB() {
+		System.out.println("Trainee B’s Hobbies:");
+		System.out.println("Sadly, none");
 	}
 }
